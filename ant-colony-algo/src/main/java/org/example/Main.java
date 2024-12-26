5155718f1520c5e3b66e563cee48d8fb565d112e
Egor Pristavka <pristavkaegor03@gmail.com>
2024-12-26 18:57:37 +0800

    Alpha ants support added

--- ant-colony-algo/src/main/java/org/example/Main.java
+++ ant-colony-algo/src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args){
         String path = "/home/mrglaster/Desktop/graphs-study/ISU-Graph-Algorithms/ant-colony-algo/src/main/resources/graph_alt.txt";
         AntColonyAlgorithm algo = new AntColonyAlgorithm();
-        algo.solve();
+        algo.solve(0.1);
     }
 }
